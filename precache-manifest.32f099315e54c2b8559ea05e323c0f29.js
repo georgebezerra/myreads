807176fcb2f9d67359c84cb230903678c46026e6
George .S. Bezerra <george.contato@gmail.com>
2018-12-29 17:33:00 -0200

    Updates

---
+++ precache-manifest.32f099315e54c2b8559ea05e323c0f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e0023cfe8834600787b3",
+    "url": "/static/js/main.e0023cfe.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e0023cfe8834600787b3",
+    "url": "/static/css/main.d14f9058.chunk.css"
+  },
+  {
+    "revision": "bbd50f866a2a100725462356a5456f12",
+    "url": "/index.html"
+  }
+];
